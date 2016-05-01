d3ff2a4f926f11f3a54ed9f29d132059a442a8f4
GMan <gary.mail.low@gmail.com>
2016-04-30 20:55:49 -0500

    Some cleanup on Orchestrator.py.  Still not operational

--- Orchestrator.py
+++ Orchestrator.py
@@ -11,11 +11,19 @@ class Orchestrator(object):
 		self.region=region 					# default to us-west-2
 		self.directiveSpecTableName='DirectiveSpec'
 		self.directiveSpecPartitionKey='SpecName'
+		self.directiveSpecDict={}
+		self.dynDBC = boto3.client('dynamodb', region_name=self.region)
+		
+
 
 		self.tierSpecTableName='TierSpecification'
 		self.tierSpecPartitionKey='SpecName'  # Same as directiveSpecPartitionKey
-		self.specDict={}
-		self.dynDBC = boto3.client('dynamodb', region_name=self.region)
+		self.tierSpecDict={}
+
+		# Table requires the DynamoDB.Resource
+		self.dynDBR = boto3.resource('dynamodb', region_name=self.region)
+		self.tierSpecTable = self.dynDBR.Table(self.tierSpecTableName)
+
 		self.initLogging()
 
 	def lookupDirectiveSpec(self, partitionTargetValue):
@@ -39,10 +47,10 @@ class Orchestrator(object):
 				attributeValue=resultItem[attributeName].values()[0]
 				#print "AttributeValue: ", attributeValue + '\n'
 
-				self.specDict[attributeName]=attributeValue
+				self.directiveSpecDict[attributeName]=attributeValue
 
-			for key, value in self.specDict.iteritems():
-				print 'specDict (key=%s, value=%s)' % (key, value)
+			for key, value in self.directiveSpecDict.iteritems():
+				print 'directiveSpecDict (key=%s, value=%s)' % (key, value)
 			#
 			#self.processItemJSON(resultItem)
 			#
@@ -54,111 +62,29 @@ class Orchestrator(object):
 			Values are attributeValues of the DDB Item Keys
 		'''
 		try:
-			dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
-			specTable = dynamodb.Table(self.tierSpecTableName)
-
-			dynamodbItem=specTable.query(
-				KeyConditionExpression=Key('SpecName').eq('BotoTestCase1'),
+			dynamodbItem=self.tierSpecTable.query(
+				KeyConditionExpression=Key(self.tierSpecPartitionKey).eq(partitionTargetValue),
 				ConsistentRead=False,
 				ReturnConsumedCapacity="TOTAL",
 			)
-
-			print dynamodbItem
+			#print dynamodbItem
 		except ClientError as e:
 			self.logger.warning(e.response['Error']['Message'])
 		else:
 			# Get the item from the result
 			resultItems=dynamodbItem['Items']
+			self.logger.debug(resultItems)
+			
+			for attribute in resultItems:
+				# print "Tier Tag Value ==>", attribute['TierTagValue']
+				# print "TierStart==>", attribute['TierStart']
+				# print '\n'
+				# print "TierStop==>", attribute['TierStop']
+				# print '\n\n'
+				self.tierSpecDict[attribute['TierTagValue']]={'TierStop' : attribute['TierStop'], 'TierStart' : attribute['TierStart']}
 
-			for i in resultItems:
-				print "Tier Tag Value ==>", i['TierTagValue']
-				print "TierStart==>", i['TierStart']
-				print '\n'
-				print "TierStop==>", i['TierStop']
-				print '\n\n'
-
-			'''
-			for attributeName in resultItems[0]:
-				print "AttributeName: ", attributeName
-				attributeValue=resultItems[attributeName].values()[0]
-				print "AttributeValue: ", attributeValue + '\n'
-
-			for attributeName in resultItems[1]:
-				print "AttributeName: ", attributeName
-				attributeValue=resultItems[attributeName].values()[0]
-				print "AttributeValue: ", attributeValue + '\n'
-
-			for attributeName in resultItems[2]:
-				print "AttributeName: ", attributeName
-				attributeValue=resultItems[attributeName].values()[0]
-				print "AttributeValue: ", attributeValue + '\n'
-			'''
-
-	def processItemJSON(self, resultItem):
-		#print "Result Item Raw: ", resultItem
-
-		# Convert DynamoDB output to readable JSON
-		resultItemAsJSON=json.dumps(resultItem, sort_keys=True, indent=4, separators=(',',':'))
-		self.logger.info("Result Item as JSON==> " + resultItemAsJSON)
-		self.logger.debug("Result Item as JSON==> " + resultItemAsJSON)
-		
-		# Convert to Python List
-		resultItemAsList=json.loads(resultItemAsJSON)
-		#print "Result Item as List: ", resultItemAsList
-		
-
-		# Convert into new SpecDictionary
-		for attributeName in resultItemAsList:
-			# print("AttributeName: ")
-			# print(attributeName)
-			attributeValue=resultItemAsList[attributeName].values()[0]
-			# print("AttributeValue: ")
-			# print(attributeValue)
-			# print('\n')
-			self.specDict[attributeName]=attributeValue
-
-		'''
-		Set the Standard Specs for convenience
-			SpecName	: the Partition Key indicating the "name" of the specification 
-			Directive 	: the action to be taken, "Start", "Stop", "Scale"
-			Region 		: the region to execute the action against
-			EnvFilterTagName 	: The Tag Name which will contain the Environment identifier
-			EnvFilterTagValue 	: The Tag Value which contains the Environment Name to filter against to locate instances
-			TierFilterTagName	: The Tag Name which will contain the Tier identifier
-			TierSequence<X> : the identifier indicating the Tag Value of the tier, and its sequence position for action  
-		'''
-		self.specName=self.specDict['SpecName']
-		logging.debug("SpecName==>" + self.specName)
-		self.specRegion=self.specDict['Region']
-		logging.debug("SpecRegion==>" + self.specRegion)
-		self.envFilterTagName=self.specDict['EnvFilterTagName']
-		logging.debug("EnvFilterTagName==>" + self.envFilterTagName)
-		self.envFilterTagValue=self.specDict['EnvFilterTagValue']
-		logging.debug("EnvFilterTagValue==>" + self.envFilterTagValue)
-
-		'''
-		Now, create an ordered list of Tier Sequences, using a Dictionary, and capture whether 
-
-		For each item in the dictionary
-			check if "TierSequence" in $instances
-				YES: 
-					parse the item to get the sequence number (e.g. "3" if "TierSequence3")
-					PUT seq# : item into TierSequenceList NOTE: insertion order into the list doesn't matter
-					list.insert(index, item) 
-					This will give you an ordered list of tiers to action upon
-
-
-		'''
-
-
-		# # Use to debug the created SpecDict
-		# for key, value in self.specDict.iteritems():
-		# 	print ('SpecDict key')
-		# 	print key
-		# 	print ('SpecDict value')
-		# 	print self.specDict[key]
-		# 	print('\n')
-
+			for key, value in self.tierSpecDict.iteritems():
+				self.logger.debug('tierSpecDict (key=%s, value=%s)' % (key, value))
 
 	def sequenceTiers(self):
 		pass
@@ -180,7 +106,7 @@ class Orchestrator(object):
 		pass
 
 
-	def scaleInstance(self):
+	def scaleInstance(self, direction):
 		pass
 
 	def initLogging(self):
@@ -196,7 +122,9 @@ class Orchestrator(object):
 
 if __name__ == "__main__":
 	orch = Orchestrator('us-west-2')
-	#orch.lookupDirectiveSpec('BotoTestCase1')
+	orch.logger.info("Executing lookupDirectiveSpec()")
+	orch.lookupDirectiveSpec('BotoTestCase1')
+	orch.logger.info("Executing lookupTierSpec()")
 	orch.lookupTierSpec('BotoTestCase1')
 	
 
